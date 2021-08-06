b9b6ab54ba9d75636df4f650ba140e16e56f3e27
Chad Purdy <cpurdy@flourbluffschools.net>
2021-08-06 15:55:53 -0500

    initial commit

---
+++ IteratorRemoverRunner.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import static java.lang.System.*;
+
+public class IteratorRemoverRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases	
+	}
+}
---
+++ IteratorReplacerRunner.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+import static java.lang.System.*;
+
+public class IteratorReplacerRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases		
+	}
+}
